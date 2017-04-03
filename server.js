29934e464dab2ab4139fb99a39abedae8515c696
coreyarch1234 <harrilal@mit.edu>
2017-04-02 17:47:58 -0700

    added npm packages

---
+++ server.js
@@ -0,0 +1,31 @@
+//Express Set
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+var bodyParser = require('body-parser');
+//Allows you to use req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+// getting-started.js Mongoose
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/contendify');
+
+
+//Jwt and cookies
+var jwt = require('express-jwt');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
+//DEPLOY
+app.listen(process.env.PORT || 3000, function(){
+
+});
+
+module.exports = app;
