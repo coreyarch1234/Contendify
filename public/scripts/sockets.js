bb7ff98ef7675071ad5ced327e2d3ba5120a1012
coreyarch1234 <harrilal@mit.edu>
2017-07-01 15:26:00 -0700

    changed timeout back to 5 sec

--- public/scripts/sockets.js
+++ public/scripts/sockets.js
@@ -9,7 +9,7 @@ var participants = 0;
 $(function() {
 
     $('.question').first().show().addClass('current-question');
-    gameCode = window.location.href.split('/')[4];
+    gameCode = window.location.href.split('/')[3];
     socket.emit('publish:join', gameCode);
 
     // // MARK: New (for waiting room)
@@ -141,6 +141,6 @@ $(function() {
           $('#answer-input').show(); // unhide input
           $('#answers').hide();
         }
-    }, 2000);
+    }, 5000);
   })
 });
